ce04c227f4de2e1e10f0f0111092ad0438b1861c
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-09-19 22:01:36 -0400

    work

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+import registerHandler from "./controllers/register.js";
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({ 
+  // connect to your own database here:
+  client: 'pg',
+  connection: {
+    host : 'dpg-d36vs80gjchc73brcnog-a.oregon-postgres.render.com',
+    user : 'smart_brain_v2ks_user',
+    password : 'rrXdrLyOD8bYVKbKCtIEV56kRuS0vs8R',
+    database : 'smart_brain_v2ks'
+  }
+});
+
+const app = express();
+
+
+app.use(cors())
+app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+
+app.get('/', (req, res)=> { res.send(db.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
