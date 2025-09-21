4c2c0384fe675979722a3300be4af41a72b06843
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-09-21 17:08:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
+const bodyParser = require('body-parser'); // Note: express.json() is sufficient for parsing JSON
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
@@ -10,39 +10,44 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({ 
-  // connect to your own database here:
   client: 'pg',
   connection: {
-    connectionString : 'postgresql://smart_brain_v2ks_user:rrXdrLyOD8bYVKbKCtIEV56kRuS0vs8R@dpg-d36vs80gjchc73brcnog-a/smart_brain_v2ks',
-    host : 'dpg-d36vs80gjchc73brcnog-a.oregon-postgres.render.com',
-    user : 'smart_brain_v2ks_user',
-    password : 'rrXdrLyOD8bYVKbKCtIEV56kRuS0vs8R',
-    database : 'smart_brain_v2ks'
+    connectionString: 'postgresql://smart_brain_v2ks_user:rrXdrLyOD8bYVKbKCtIEV56kRuS0vs8R@dpg-d36vs80gjchc73brcnog-a/smart_brain_v2ks',
+    host: 'dpg-d36vs80gjchc73brcnog-a.oregon-postgres.render.com',
+    user: 'smart_brain_v2ks_user',
+    password: 'rrXdrLyOD8bYVKbKCtIEV56kRuS0vs8R',
+    database: 'smart_brain_v2ks'
   }
 });
 
-
-;
-
-
-const app = express();
-app.use(cors({
-  origin: 'https://smart-brain-frontend-7xlb.onrender.com'
-}));
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 const { handleApiCall, handleImage } = require('./controllers/image');
 
-app.put('/image', handleImage);
-app.post('/imageurl', handleApiCall);
+const app = express();
 
+app.use(cors({
+  origin: 'https://smart-brain-frontend-7xlb.onrender.com' // your frontend URL
+}));
+app.use(express.json()); // built-in body parser
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+// Routes
+app.get('/', (req, res) => { res.send('Server is running') });
 
-app.listen(3000, ()=> {
+// Sign in route
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
+
+// Register route
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+
+// Profile route
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
+
+// Update user entries
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+
+// Face detection API call
+app.post('/imageurl', handleApiCall); // Using imported function directly
+
+// Start server
+app.listen(3000, () => {
   console.log('app is running on port 3000');
-})
+});
