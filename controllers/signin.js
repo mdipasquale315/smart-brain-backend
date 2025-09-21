2ae9a515b48582cbf64b3ad95cc94905e23c540a
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-09-21 19:14:19 -0400

    Update signin.js

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,26 +1,49 @@
 const handleSignin = (db, bcrypt) => (req, res) => {
   const { email, password } = req.body;
+
   if (!email || !password) {
-    return res.status(400).json('incorrect form submission');
+    return res.status(400).json('Incorrect form submission');
   }
-  db.select('email', 'hash').from('login')
+
+  // Find the email and hash from login table
+  db.select('email', 'hash')
+    .from('login')
     .where('email', '=', email)
     .then(data => {
-      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (data.length === 0) {
+        return res.status(400).json('Wrong credentials');
+      }
+
+      const hash = data[0].hash;
+
+      // Check password validity
+      const isValid = bcrypt.compareSync(password, hash);
       if (isValid) {
-        return db.select('*').from('users')
+        // Fetch user info from users table
+        return db.select('*')
+          .from('users')
           .where('email', '=', email)
           .then(user => {
-            res.json(user[0])
+            if (user.length) {
+              res.json(user[0]);
+            } else {
+              res.status(400).json('Unable to get user');
+            }
           })
-          .catch(err => res.status(400).json('unable to get user'))
+          .catch(err => {
+            console.error('Error fetching user:', err);
+            res.status(500).json('Unable to get user');
+          });
       } else {
-        res.status(400).json('wrong credentials')
+        res.status(400).json('Wrong credentials');
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
-}
+    .catch(err => {
+      console.error('Error during login:', err);
+      res.status(500).json('Internal server error');
+    });
+};
 
 module.exports = {
-  handleSignin: handleSignin
-}
+  handleSignin
+};
