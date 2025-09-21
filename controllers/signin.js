e736360e80265410b0282027ede75076d80429dd
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-09-21 17:47:17 -0400

    Update signin.js

--- controllers/signin.js
+++ controllers/signin.js
@@ -6,21 +6,30 @@ const handleSignin = (db, bcrypt) => (req, res) => {
   db.select('email', 'hash').from('login')
     .where('email', '=', email)
     .then(data => {
+      if (data.length === 0) {
+        return res.status(400).json('wrong credentials');
+      }
       const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
         return db.select('*').from('users')
           .where('email', '=', email)
           .then(user => {
-            res.json(user[0])
+            if (user.length) {
+              res.json(user[0]);
+            } else {
+              res.status(400).json('unable to get user');
+            }
           })
-          .catch(err => res.status(400).json('unable to get user'))
+          .catch(err => {
+            console.error('Error fetching user:', err);
+            res.status(400).json('unable to get user');
+          });
       } else {
-        res.status(400).json('wrong credentials')
+        res.status(400).json('wrong credentials');
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
-}
-
-module.exports = {
-  handleSignin: handleSignin
-}
+    .catch(err => {
+      console.error('Error during sign in:', err);
+      res.status(400).json('wrong credentials');
+    });
+};
