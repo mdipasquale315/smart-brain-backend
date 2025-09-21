44bd5150a612a31bd27fb393be5921f0682b12bc
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-09-21 18:58:38 -0400

    Update signin.js

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,48 +1,26 @@
-// Assuming you're using knex for db and bcryptjs for hashing
 const handleSignin = (db, bcrypt) => (req, res) => {
   const { email, password } = req.body;
-
-  // Basic validation
   if (!email || !password) {
-    return res.status(400).json('Incorrect form submission');
+    return res.status(400).json('incorrect form submission');
   }
-
-  // Find the user login info by email
-  db.select('email', 'hash')
-    .from('login')
+  db.select('email', 'hash').from('login')
     .where('email', '=', email)
     .then(data => {
-      if (data.length === 0) {
-        // Email not found
-        return res.status(400).json('Wrong credentials');
-      }
-
-      const hash = data[0].hash;
-
-      // Compare provided password with hash
-      const isValid = bcrypt.compareSync(password, hash);
-
+      const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
-        // Fetch user info
-        return db.select('*')
-          .from('users')
+        return db.select('*').from('users')
           .where('email', '=', email)
           .then(user => {
-            if (user.length) {
-              res.json(user[0]);
-            } else {
-              res.status(400).json('Unable to get user');
-            }
-          });
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
       } else {
-        // Password mismatch
-        return res.status(400).json('Wrong credentials');
+        res.status(400).json('wrong credentials')
       }
     })
-    .catch(err => {
-      console.error('Error during sign-in:', err);
-      res.status(500).json('Internal server error');
-    });
-};
+    .catch(err => res.status(400).json('wrong credentials'))
+}
 
-module.exports = { handleSignin };
+module.exports = {
+  handleSignin: handleSignin
+}
