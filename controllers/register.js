813220cf3dfc5655c86863c7f86186802d2f6be9
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-09-22 13:30:44 -0400

    Update register.js

--- controllers/register.js
+++ controllers/register.js
@@ -1,31 +1,34 @@
-const registerHandler = (req, res, db, bcrypt) => {
+const registerHandler = async (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
-    return res.status(400).json('incorrect form submission');
+    return res.status(400).json('Incorrect form submission');
   }
-  const hash = bcrypt.hashSync(password);
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-      return trx('users')
+
+  try {
+    const hash = await bcrypt.hash(password, null, null); // bcrypt.hash is async
+    await db.transaction(async trx => {
+      // Insert into login table
+      await trx('login')
+        .insert({
+          email: email,
+          hash: hash
+        });
+
+      // Insert into users table
+      const user = await trx('users')
         .returning('*')
         .insert({
-          email: loginEmail[0].email,
+          email: email,
           name: name,
           joined: new Date()
-        })
-        .then(user => {
-          res.json(user[0]);
         });
-    })
-    .then(trx.commit)
-    .catch(trx.rollback);
-  }).catch(err => res.status(400).json('unable to register'));
+
+      res.json(user[0]);
+    });
+  } catch (err) {
+    console.error('Registration error:', err);
+    res.status(400).json('Unable to register');
+  }
 };
 
 export default registerHandler;
