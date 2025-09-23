c35f3f15a6dcdcd68c90e6e41ac758bbca11a843
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-09-23 12:21:19 -0400

    Update profile.js

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,17 +1,16 @@
 import fetch from 'node-fetch';
+
 const handleProfileGet = (req, res, db) => {
   const { id } = req.params; 
   db.select('*').from('users').where({id})
     .then(user => {
       if (user.length) {
-        res.json(user[0])
+        res.json(user[0]);
       } else {
-        res.status(400).json('Not found')
+        res.status(400).json('Not found');
       }
     })
-    .catch(err => res.status(400).json('error getting user'))
-}
+    .catch(err => res.status(400).json('error getting user'));
+};
 
-module.exports = {
-  handleProfileGet
-}
+export { handleProfileGet };
