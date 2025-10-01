5c85a89dc7395d59df87c97d6e59f27ba76bff9b
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-10-01 19:52:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const signup = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const signup = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
+
 const saltRounds = 10;
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 const db = knex({ 
   client: 'pg',
   connection: {
@@ -13,13 +19,14 @@ const db = knex({
     ssl: { rejectUnauthorized: false }
   }
 });
+
 app.get('/', (req, res) => { res.json("Welcome to Face Detection API...") });
 // Dependency Injection
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 app.post('/signup', (req, res) => { signup.handleSignup(req, res, db, bcrypt, saltRounds) });
 app.get('/profile/:userId', (req, res) => { profile.handleGetProfile(req, res, db) });
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });  // FIXED THIS LINE
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 // Start server
 app.listen(3000, () => {
   console.log('app is running on port 3000');
