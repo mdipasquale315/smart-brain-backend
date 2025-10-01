a462e73934396143e521b04f61c29ef6ae3f0f8f
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-10-01 19:14:05 -0400

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -1,24 +1,23 @@
-const APP_ID = 'face-detection';
+const handleApiCall = (req, res) => {
+    const APP_ID = 'face-detection';
     const MODEL_ID = 'face-detection';
     const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
     const IMAGE_URL = req.body.input;
-
     const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
-
     const stub = ClarifaiStub.grpc();
-
+    
     // This will be used by every Clarifai endpoint call
     const metadata = new grpc.Metadata();
-    metadata.set("authorization", "Key " + PAT);
-
-    stub.PostModelOutputs(
+    metadata.set("authorization", "Key " + process.env.CLARIFAI_API_KEY); // Use environment variable
+    
+    stub.postModelOutputs(  // Changed from PostModelOutputs to postModelOutputs
         {
             user_app_id: {
-                "user_id": '9d971wpgvajg', ,
+                "user_id": '9d971wpgvajg',  // Removed extra comma
                 "app_id": 'face-detection'
             },
             model_id: MODEL_ID,
-            version_id: MODEL_VERSION_ID, // This is optional. Defaults to the latest model version
+            version_id: MODEL_VERSION_ID,
             inputs: [
                 { data: { image: { url: IMAGE_URL, allow_duplicate_url: true } } }
             ]
@@ -26,25 +25,17 @@ const APP_ID = 'face-detection';
         metadata,
         (err, response) => {
             if (err) {
-                throw new Error(err);
+                console.error("Clarifai API Error:", err);
+                return res.status(400).json("Error calling Clarifai API");
             }
-
             if (response.status.code !== 10000) {
-                // console.log(IMAGE_URL)
-                throw new Error("Post model outputs failed, status: " + response.status.description);
+                console.error("Clarifai Response Error:", response.status.description);
+                return res.status(400).json("Post model outputs failed: " + response.status.description);
             }
-
-            // Since we have one input, one output will exist here
+            
             const output = response.outputs[0];
-
-            // console.log("Predicted concepts:");
-            // for (const concept of output.data.concepts) {
-            //     console.log(concept.name + " " + concept.value);
-            // }
-            // console.log(output)
-            return res.json(output)
+            return res.json(output);
         }
-
     );
 }
 
@@ -65,5 +56,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
     handleImage,
-    clarifaiRequest
+    handleApiCall  // Changed from clarifaiRequest
 }
