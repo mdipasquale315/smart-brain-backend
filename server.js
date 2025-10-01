4780f1ab1dc8500f818a6685ef9ce14c10035194
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-10-01 19:19:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ const signup = require('./controllers/signup');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-
 const saltRounds = 10;
-
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 const db = knex({ 
   client: 'pg',
   connection: {
@@ -16,18 +13,13 @@ const db = knex({
     ssl: { rejectUnauthorized: false }
   }
 });
-
-
 app.get('/', (req, res) => { res.json("Welcome to Face Detection API...") });
-
 // Dependency Injection
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 app.post('/signup', (req, res) => { signup.handleSignup(req, res, db, bcrypt, saltRounds) });
 app.get('/profile/:userId', (req, res) => { profile.handleGetProfile(req, res, db) });
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
-app.post('/imageurl', (req, res) => { image.clarifaiRequest(req, res) });
-
-
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });  // FIXED THIS LINE
 // Start server
 app.listen(3000, () => {
   console.log('app is running on port 3000');
