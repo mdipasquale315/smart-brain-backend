f83242cd310900a8029a23c48274e68a3ab85882
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-10-01 20:33:46 -0400

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -8,7 +8,7 @@ const handleApiCall = (req, res) => {
     
     const stub = ClarifaiStub.grpc();
     const metadata = new grpc.Metadata();
-    metadata.set("authorization", "Key " + process.env.CLARIFAI_API_KEY);
+    metadata.set("authorization", "Key " + b27bf519c2414f5aabffbe6e5a532bf1);
     
     stub.postModelOutputs(
         {
